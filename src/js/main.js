8ac2ee6f52ab5f782ec602a6dfde1708e7f8db2d
FlyingFreestyle <139637440+FlyingFreestyle@users.noreply.github.com>
2023-08-24 17:21:19 +0200

    Add classes to html. Add JS load and save features

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,25 @@ function sumArray(array) {
 
 const arr = [1, 2, 3, 4, 5]
 console.log(sumArray(arr))
+
+const message = document.querySelector('.message--js')
+
+if (localStorage["content"])
+{
+    console.log('Content already exists. Loading...')
+    let content = localStorage["content"] ;
+    message.value = content ;
+    console.log('Data read from previous visit')
+}
+
+const saveButton = document.querySelector('.save-ls--js')
+saveButton.addEventListener('click', () => {
+    localStorage.setItem('content', message.value)
+    console.log("Data saved to Local Storage")
+})
+
+const loadButton = document.querySelector('.load-ls--js')
+loadButton.addEventListener('click', () => {
+    message.value = localStorage.getItem('content')
+    console.log("Data read from Local Storage")
+})
